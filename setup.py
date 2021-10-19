d661a1892afa19193fcb97b7c157129668d6b553
miglegervyte <33330541+miglegervyte@users.noreply.github.com>
2021-10-19 16:48:45 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
    description='Calculator that performs basic mathematical operations',
    author='Migle Gervyte',
    author_email='migle.gervyte@gmail.com',
-   packages=['Calculator'],
+   packages=['Calculator', 'test'],
    licence='MIT',
    url='https://github.com/miglegervyte/Calculator',
    install_requires=['pytest'],  # external packages as dependencies
